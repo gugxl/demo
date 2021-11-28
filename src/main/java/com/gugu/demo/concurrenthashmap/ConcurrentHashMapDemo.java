224f3bf9db8e92ecc6e93351d58243bba7eab22e
gugu <gu1230gu1230@gmail.com>
2021-11-28 14:09:18 +0800

    ConcurrentHashMap 递归创建对象bug  [jdk1.8]

---
+++ src/main/java/com/gugu/demo/concurrenthashmap/ConcurrentHashMapDemo.java
@@ -0,0 +1,29 @@
+package com.gugu.demo.concurrenthashmap;
+
+import java.security.Key;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author Administrator
+ * @Classname ConcurrentHashMapDemo
+ * @Description TODO
+ * @Date 2021/11/28 13:55
+ */
+public class ConcurrentHashMapDemo {
+    private Map<Integer, Integer> cache =  new ConcurrentHashMap<>(15);
+    public static void main(String[] args) {
+        ConcurrentHashMapDemo concurrentHashMapDemo = new ConcurrentHashMapDemo();
+        System.out.println(concurrentHashMapDemo.fibonaacci(80));
+    }
+
+    public int fibonaacci(Integer i){
+        if(i==0||i ==1) {
+            return i;
+        }
+        return cache.computeIfAbsent(i, key -> {
+            System.out.println("fibonaacci : "+ key);
+            return fibonaacci(i-1) + fibonaacci(i-2);
+        });
+    }
+}
