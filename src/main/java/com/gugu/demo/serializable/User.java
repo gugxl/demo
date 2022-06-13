fcb5ae894ba4d6edeb1ef52a105e2e2c72e3cd8a
gugu <gu1230gu1230@gmail.com>
2022-06-13 23:29:07 +0800

    简单自定义ClassLoader
    Signed-off-by: gugu <gu1230gu1230@gmail.com>

--- src/main/java/com/gugu/demo/serializable/User.java
+++ src/main/java/com/gugu/demo/serializable/User.java
@@ -12,7 +12,16 @@ public class User implements Serializable {
 
     private static final long serialVersionUID = -8840853971861469954L;
 
-    private String userName;
+    public User() {
+        System.out.println("init User");
+    }
+
+    public User(String userName, String address) {
+        this.userName = userName;
+        this.address = address;
+    }
+
+    private String userName = "gugu";
     // transient 关键词修饰的属性不会序列化
     private transient String address;
 
