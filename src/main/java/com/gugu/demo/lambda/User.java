2e26d93eb4ac21c3db3ad72a663e8ea1d4f3e64c
gugu <gu1230gu1230@gmail.com>
2023-05-15 23:29:46 +0800

    ::基本使用
    Signed-off-by: gugu <gu1230gu1230@gmail.com>

---
+++ src/main/java/com/gugu/demo/lambda/User.java
@@ -0,0 +1,18 @@
+package com.gugu.demo.lambda;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private String username;
+    private Integer age;
+
+    public User() {
+    }
+
+    public User(String username, Integer age) {
+        this.username = username;
+        this.age = age;
+    }
+
+}
